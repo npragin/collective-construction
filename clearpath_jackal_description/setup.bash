0dfff86cc3f43a50f8381a1911422f62d844fc71
DigeshC <chitrakar.digesh@gmail.com>
2026-05-19 15:02:04 -0700

    Updated robot.yaml file

--- clearpath_jackal_description/setup.bash
+++ clearpath_jackal_description/setup.bash
@@ -1,9 +1,9 @@
-# Bash setup generated at 2026-05-16 02:22:57.979822+00:00
+# Bash setup generated at 2026-05-19 15:49:37.168181+00:00
 # Core ROS setup
 source /opt/ros/jazzy/setup.bash
 # ROS configuration
 export ROS_DOMAIN_ID="0"
-export RMW_IMPLEMENTATION="rmw_zenoh_cpp"
+export RMW_IMPLEMENTATION="rmw_fastrtps_cpp"
 unset ROS_DISCOVERY_SERVER
 export ROS_AUTOMATIC_DISCOVERY_RANGE="SUBNET"
 unset ROS_STATIC_PEERS
