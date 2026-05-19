4acaa7b67af38c67f9db14ff3907638a005a27a3
trueog1 <gehrkeo@oregonstate.edu>
2026-05-19 00:29:12 +0000

    Jackal nav stack files

---
+++ nav/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'nav'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='trueog',
+    maintainer_email='gehrkeo@oregonstate.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        'waypoint_follower =nav.waypoint_follower_simple:main',
+        ],
+    },
+)
