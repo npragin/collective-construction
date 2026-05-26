2ae003c5f752775b871cde88eed5d3911073f956
itachity <santosmatthewjohn@gmail.com>
2026-05-25 17:24:44 -0700

    update launch file

--- manipulator_perception/launch/aruco_realsense.launch.py
+++ manipulator_perception/launch/aruco_realsense.launch.py
@@ -39,14 +39,14 @@ def generate_launch_description():
                 "color_topic": "/j100_0897/sensors/camera_0/color/image",
                 "depth_topic": "/j100_0897/sensors/camera_0/depth/image",
                 "camera_info_topic": "/j100_0897/sensors/camera_0/color/camera_info",
-                "marker_size": 0.05,
-                "aruco_dictionary": "original",
+                "marker_size": 0.10,
+                "aruco_dictionary": "4x4_50",
                 "target_id": -1,
                 "publish_tf": True,
                 "publish_rviz_markers": True,
                 "arm_base_frame": "arm_0_base_link",
                 "world_frame": "base_link",
-                "show_window": False,
+                "show_window": True,
             }
         ]
     )
