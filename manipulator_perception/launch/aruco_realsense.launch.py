cf9cb6ea69c1cff7edb8076412a0b249021d2ed2
itachity <santosmatthewjohn@gmail.com>
2026-05-25 17:30:36 -0700

    update launch file

--- manipulator_perception/launch/aruco_realsense.launch.py
+++ manipulator_perception/launch/aruco_realsense.launch.py
@@ -8,27 +8,6 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
-    realsense_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([
-                FindPackageShare("realsense2_camera"),
-                "launch",
-                "rs_launch.py"
-            ])
-        ),
-        launch_arguments={
-            "align_depth.enable": "true",
-            "enable_color": "true",
-            "enable_depth": "true",
-            "enable_infra1": "false",
-            "enable_infra2": "false",
-            "enable_gyro": "false",
-            "enable_accel": "false",
-            "rgb_camera.profile": "640x480x15",
-            "depth_module.profile": "640x480x15",
-        }.items()
-    )
-
     aruco_node = Node(
         package="aruco_depth_ros2",
         executable="aruco_depth_node",
@@ -52,6 +31,5 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        realsense_launch,
         aruco_node,
     ])
