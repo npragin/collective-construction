6880651906797d01031b7b19da3a256ed5129a47
Liam Bouffard <liamtbo@gmail.com>
2026-05-27 16:14:02 -0700

    working on combingin conor and my work

--- scouts/launch/scouts_launch.py
+++ scouts/launch/scouts_launch.py
@@ -77,17 +77,17 @@ def generate_launch_description():
             ],
         ),
 
-        # # RealSense camera
-        # IncludeLaunchDescription(
-        #     PythonLaunchDescriptionSource(realsense_launch),
-        #     launch_arguments={
-        #         "initial_reset": "true",
-        #         "camera_namespace": namespace,
-        #         "depth_module.depth_profile": "1280x720x30",
-        #         "rgb_camera.color_profile": "1280x720x30",
-        #         "enable_sync": "true",
-        #     }.items(),
-        # ),
+        # RealSense camera
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(realsense_launch),
+            launch_arguments={
+                "initial_reset": "true",
+                "camera_namespace": namespace,
+                "depth_module.depth_profile": "1280x720x30",
+                "rgb_camera.color_profile": "1280x720x30",
+                "enable_sync": "true",
+            }.items(),
+        ),
 
         # Node(
         #     package='scouts',
@@ -97,12 +97,12 @@ def generate_launch_description():
         #     output='screen'
         # )
 
-        # Node(
-        #     package="scouts",
-        #     executable="waypoint_server",
-        #     name="waypoint_server",
-        #     namespace=namespace,
-        #     output="screen",
-        # ),
+        Node(
+            package="scouts",
+            executable="waypoint_server",
+            name="waypoint_server",
+            namespace=namespace,
+            output="screen",
+        ),
 
     ])
