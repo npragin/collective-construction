2a145ef772152e265de1a4b83dc79b00e1abdaa6
DigeshC <chitrakar.digesh@gmail.com>
2026-06-01 15:02:51 -0700

    Update Robot URDF

--- clearpath_jackal_description/setup.bash
+++ clearpath_jackal_description/setup.bash
@@ -1,8 +1,9 @@
-# Bash setup generated at 2026-05-25 23:32:02.919342+00:00
+# Bash setup generated at 2026-06-01 22:00:50.513393+00:00
 # Core ROS setup
 source /opt/ros/jazzy/setup.bash
 # Additional workspaces
 source /home/robot/ws/ros2_kinova/install/setup.bash
+source /home/robot/ws/rob599/install/setup.bash
 # ROS configuration
 export ROS_DOMAIN_ID="0"
 export RMW_IMPLEMENTATION="rmw_fastrtps_cpp"
