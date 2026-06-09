7fe5356770f50f9c3b6ce71fe08a19ed74c1c734
Liam Bouffard <liamtbo@gmail.com>
2026-06-08 20:12:54 -0700

    working on localizing

--- scouts/launch/navigation_launch.py
+++ scouts/launch/navigation_launch.py
@@ -211,9 +211,11 @@ def generate_launch_description():
                 respawn_delay=2.0,
                 parameters=[configured_params],
                 arguments=['--ros-args', '--log-level', log_level],
+                # remappings=remappings
+                # # + [('cmd_vel', 'cmd_vel_nav')],
+                # + [('cmd_vel', 'sierra/cmd_vel')], # TODO
                 remappings=remappings
-                # + [('cmd_vel', 'cmd_vel_nav')],
-                + [('cmd_vel', 'sierra/cmd_vel')], # TODO
+                + [('cmd_vel_smoothed', 'cmd_vel_nav'), ('cmd_vel', '/sierra/cmd_vel')],
             ),
             # Node(
             #     package='nav2_collision_monitor',
